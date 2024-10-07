51e7fc567a863a3e6c11185c7df72fd6b830c48d
Ishan Gupta <ishangupta.code@gmail.com>
2024-10-07 10:35:47 +0530

    File add

---
+++ OOPS.py
@@ -0,0 +1,31 @@
+# Class --> It is a collection of object and function.
+# Object --> It is the 
+
+# Class Name is always in CamelCase
+class ishan:
+    print("My name is Ishan Gupta")
+    age = 18
+    email = "Temp@gmail.com"
+
+# Object Creation
+# object : Class = Class
+isha : ishan = ishan()
+print("My name is:",isha.age)
+print("My email I'D:",isha.email)
+
+class Student:
+    # Objects
+    name ="ishan"
+    email = "temp@gamil.com"
+    # Function
+    def findMyAge(this, cY,bY):  # "this" -> used to give current class reference
+        ageInYear = cY - bY
+        print("My age is:",ageInYear)
+    def monthlyPocketMoney(this,weeklyMoney):
+        pocketMoney = weeklyMoney * 4
+        print("Monthly Pocket Money is:",pocketMoney)
+
+
+obj:Student  = Student()
+obj.findMyAge(2024,2005)
+obj.monthlyPocketMoney(200)
